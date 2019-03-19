96c73a2a60d300e6cf8044ba9499e9932b918f92
azeem sarwar <azeem.sarwar@nadra.gov.pk>
2019-03-19 12:52:36 +0500

    3

---
+++ app/models/bug.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+var bugCtrl = require('../controllers/bug_controller');
+
+router.post('/:componentId',bugCtrl.addBug);
+router.get('/:id',bugCtrl.getBug);
+router.get('/:componentId',bugCtrl.getBugs);
+router.put('/:id',bugCtrl.updateBug);
+router.delete('/:id',bugCtrl.deleteBug);
---
+++ app/routes/component_routes.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+var cmptCtrl = require('../controllers/component_controller');
+
+router.post('/:projId',cmptCtrl.addComponent);
+router.get('/:id',cmptCtrl.getComponent);
+router.get('/:projId',cmptCtrl.getComponents);
+router.put('/:id',cmptCtrl.updateComponent);
