5f0d752202db2fd52e46f47921c62a394ca3f4b2
azeem sarwar <azeem.sarwar@nadra.gov.pk>
2019-03-19 12:07:54 +0500

    More files added

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -3,10 +3,16 @@ var passport = require('passport');
 
 var router = express.Router();
 
-router.use('/boards', passport.authenticate('jwt', { session: false }), require('./board_routes'));
+// router.use('/boards', passport.authenticate('jwt', { session: false }), require('./board_routes'));
+// router.use('/users', require('./user_routes'));
+// router.use('/tasks', passport.authenticate('jwt', { session: false }), require('./task_routes'));
+// router.use('/invitations', passport.authenticate('jwt', { session: false }), require('./invitation_routes'));
+// router.use('/aws', passport.authenticate('jwt', { session: false }), require('./aws_routes'));
+
 router.use('/users', require('./user_routes'));
-router.use('/tasks', passport.authenticate('jwt', { session: false }), require('./task_routes'));
-router.use('/invitations', passport.authenticate('jwt', { session: false }), require('./invitation_routes'));
-router.use('/aws', passport.authenticate('jwt', { session: false }), require('./aws_routes'));
+router.use('/projects',passport.authenticate('jwt',{session:false}),require('./'));
+router.use('/componants',passport.authenticate('jwt',{session:false}),require('./'));
+router.use('/bugs',passport.authenticate('jwt',{session:false}),require('./'));
+
 
 module.exports = router;
